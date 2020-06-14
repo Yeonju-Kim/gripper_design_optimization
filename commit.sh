4b7d282c625a084524d3ab51c692be3eeb21d7df
zherong <zherong@cs.unc.edu>
2020-06-13 22:12:12 -0500

    renamed grasp metric path

--- commit.sh
+++ commit.sh
@@ -2,11 +2,11 @@ sudo rm -rdf data
 rm -rdf *.user
 rm -rdf *.pyc
 rm -rdf __pycache__
-cd GraspMetric
+cd ComputeMetric
 sudo mv build dist pyGraspMetric.egg-info ../../
 cd ..
 git checkout master
 git add --all
 git commit -m "$1"
 git push -u origin master
-sudo mv ../build ../dist ../pyGraspMetric.egg-info ./GraspMetric
+sudo mv ../build ../dist ../pyGraspMetric.egg-info ./ComputeMetric
--- commit_zherong.sh
+++ commit_zherong.sh
@@ -2,12 +2,12 @@ sudo rm -rdf data
 rm -rdf *.user
 rm -rdf *.pyc
 rm -rdf __pycache__
-cd GraspMetric
+cd ComputeMetric
 sudo mv build dist pyGraspMetric.egg-info ../../
 cd ..
 git checkout zherong
 git add --all
 git commit -m "$1"
 git push -u origin zherong
-sudo mv ../build ../dist ../pyGraspMetric.egg-info ./GraspMetric
+sudo mv ../build ../dist ../pyGraspMetric.egg-info ./ComputeMetric
 
