3caaf2db5fb236e4e2a03db84c632f8de773de14
zherong <zherong@cs.unc.edu>
2020-06-13 21:35:30 -0500

    added grasp metric computer's c++ side lib

--- commit.sh
+++ commit.sh
@@ -1,4 +1,7 @@
 sudo rm -rdf data
+rm -rdf *.user
+rm -rdf *.pyc
+rm -rdf __pycache__
 git checkout master
 git add --all
 git commit -m "$1"
--- commit_zherong.sh
+++ commit_zherong.sh
@@ -1,4 +1,7 @@
 sudo rm -rdf data
+rm -rdf *.user
+rm -rdf *.pyc
+rm -rdf __pycache__
 git checkout zherong
 git add --all
 git commit -m "$1"
