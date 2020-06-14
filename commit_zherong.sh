e32856fef24a447e9342582282c897f8bc453df1
zherong <zherong@cs.unc.edu>
2020-06-13 21:43:36 -0500

    refined commit script

--- commit_zherong.sh
+++ commit_zherong.sh
@@ -3,13 +3,11 @@ rm -rdf *.user
 rm -rdf *.pyc
 rm -rdf __pycache__
 cd GraspMetric
-sudo mv build ../../
-sudo mv dist ../../
-sudo mv pyGraspMetric.egg-info ../../
+sudo mv build dist pyGraspMetric.egg-info ../../
 cd ..
-sudo mv ../build ../dist ../pyDiffNE.egg-info ./GraspMetric
 git checkout zherong
 git add --all
 git commit -m "$1"
 git push -u origin zherong
+sudo mv ../build ../dist ../pyDiffNE.egg-info ./GraspMetric
 
